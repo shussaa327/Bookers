2dcaa976fc52bafe079e30674b85f57f2c293399
shunsuke sasaki <shunsukesasaki@shunsukenoMacBook-Pro.local>
2019-11-14 18:42:56 +0900

    [Add] 新規投稿、一覧表示機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,11 +3,25 @@ class BooksController < ApplicationController
   end
 
   def index
+  	@books = Book.all
+  	@book = Book.new
+  end
+
+  def create
+  	book = Book.new(book_params)
+  	book.save
+  	redirect_to book_path(book)
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def book_params
+  	params.require(:book).permit(:title, :body)
   end
 end
